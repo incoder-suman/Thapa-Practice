1728e3abb4add2da9d48871d51e1c7451b4e0a48
Suman kumar <sumanking467@gmail.com>
2025-04-24 00:27:14 +0530

    Added datatype folder

---
+++ datatype/datatype.js
@@ -0,0 +1,23 @@
+// var myFavnum = "10";
+// console.log(myFavnum);
+// console.log(typeof +myFavnum);
+// console.log(typeof Number(myFavnum)); // to convert number to string 
+
+// var myName  = "suman";
+// if (myName) {
+//     console.log("this is truthy value");    
+// } else {
+//     console.log("its falsy value");    
+// }  /// to check truthy / falsy value
+
+// dosomething()
+// .then(result1 => dosomething(result1))
+// .then(result2 =>)
+
+if (NaN === NaN) {
+    console.log("Both are wqual");
+    
+} else{
+    console.log("Not equal");
+    
+}
