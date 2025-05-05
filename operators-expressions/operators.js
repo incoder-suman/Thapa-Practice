5e8473f76784a228e080b58a2d293d3c2687a9f5
Suman kumar <sumanking467@gmail.com>
2025-05-06 01:09:31 +0530

    Learning

--- operators-expressions/operators.js
+++ operators-expressions/operators.js
@@ -96,3 +96,4 @@
 //? Write a programe to check if a number is prime 
 //? todo prime numbers are numbers that have only 
 //? All prime number greater than 2 are odd.
+//? However not all odd numbers are prime.
